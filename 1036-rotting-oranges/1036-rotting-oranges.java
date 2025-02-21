3060258e93a4e2f1b1682f01bcab66426995dfc9
Gyuhyoung Lim <do0ob1124@gmail.com>
2025-02-21 10:29:15 -0500

    Time: 2 ms (70.94%), Space: 42.4 MB (74.51%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,60 +1,63 @@
 class Solution {
     int n;
     int m;
+
+    public boolean isValid(int i, int j, int[][] grid) {
+        if(i < 0 || j < 0 || i == n || j == m) return false;
+        if(grid[i][j] != 1) return false;
+
+        return true;
+    }
+    
     public int orangesRotting(int[][] grid) {
         n = grid.length;
         m = grid[0].length;
 
         Queue<int[]> queue = new LinkedList<>();
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 2) queue.add(new int[] {i, j});
+                if(grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                }
             }
         }
 
-        int cnt = 0;
+        int time = 0;
         while(!queue.isEmpty()) {
             int size = queue.size();
-            cnt++;
-            for(int s = 0; s < size; s++) {
+            time++;
+            for(int i = 0; i < size; i++) {
                 int[] pos = queue.poll();
-                int i = pos[0];
-                int j = pos[1];
-                if(isValid(grid, i-1, j)) {
-                    queue.add(new int[]{i-1, j});
-                    grid[i-1][j] = 2;
+                int x = pos[0];
+                int y = pos[1];
+                if(isValid(x+1, y, grid)) {
+                    grid[x+1][y] = 2;
+                    queue.add(new int[]{ x+1, y });
                 }
-                if(isValid(grid, i+1, j)){ 
-                    queue.add(new int[]{i+1, j});
-                    grid[i+1][j] = 2;
+                if(isValid(x-1, y, grid)) {
+                    grid[x-1][y] = 2;
+                    queue.add(new int[]{ x-1, y });
                 }
-                if(isValid(grid, i, j-1)) {
-                    queue.add(new int[]{i, j-1});
-                    grid[i][j-1] = 2;
+                if(isValid(x, y+1, grid)) {
+                    grid[x][y+1] = 2;
+                    queue.add(new int[]{x, y+1});
                 }
-                if(isValid(grid, i, j+1)) {
-                    queue.add(new int[]{i, j+1});
-                    grid[i][j+1] = 2;
+                if(isValid(x, y-1, grid)) {
+                    grid[x][y-1] = 2;
+                    queue.add(new int[]{x, y-1});
                 }
-            } 
-            
+            }
         }
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1) return -1;
+                if(grid[i][j] == 1) {
+                    return -1;
+                }
             }
         }
 
-        if(cnt == 0) return 0;
-        return cnt -1;
+        return (time-1) == -1 ? 0 : time -1;
     }
-
-    private boolean isValid(int[][] grid, int i, int j) {
-        if(i < 0 || i == n || j < 0 || j == m) return false;
-
-        if(grid[i][j] == 1) return true;
-        
-        return false;
-    } 
 }
