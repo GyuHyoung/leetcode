6aa91061bc94c92645fafd89833135e18d297c67
Gyuhyoung Lim <do0ob1124@gmail.com>
2025-02-24 11:05:00 -0500

    Time: 8 ms (56.56%), Space: 56.3 MB (87.78%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,46 +1,44 @@
-//  a  b. a. b. a
-// p1.          p2
-// a b a b a
-// ababa
-// a bab a 
-
-//  b.  a.  a.  b.  a.  b
-//.             i.       
-
 class Solution {
-    List<List<String>> result = new ArrayList<>();
-
-    public boolean isPalindrome(String subStr) {
-        char[] charArr = subStr.toCharArray();
-
+    List<List<String>> result;
+    public boolean isPalindrome(String s) {
+        char[] charArr = s.toCharArray();
         int l = 0;
         int r = charArr.length - 1;
+
         while(l < r) {
             if(charArr[l] != charArr[r]) return false;
             l++;
             r--;
         }
-
         return true;
     }
 
-    public void dfs(int idx, String s, List<String> list) {
+    public void dfs(String s, List<String> subList) {
         if(s.length() == 0) {
-            result.add(new ArrayList(list));
-            return;
+            result.add(new ArrayList(subList));
         }
-        for(int i = idx; i < s.length(); i++) {
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
             String subStr = s.substring(0, i+1);
             if(isPalindrome(subStr)) {
-                list.add(subStr);
-                dfs(0, s.substring(i+1, s.length()), list);
-                list.remove(list.size() - 1);
+                subList.add(subStr);
+                dfs(s.substring(i+1, n), subList);
+                subList.remove(subList.size() - 1);
             } 
         }
     }
 
     public List<List<String>> partition(String s) {
-        dfs(0, s, new ArrayList<>());
+        int n = s.length();
+        result = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            String subStr = s.substring(0, i+1);
+            if(isPalindrome(subStr)) {
+                List<String> subList = new ArrayList<>();
+                subList.add(subStr);
+                dfs(s.substring(i+1, n), subList);
+            } 
+        }
 
         return result;
     }
