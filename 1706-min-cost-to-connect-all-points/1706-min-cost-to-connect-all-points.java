ce699f25522c7bcebc9a2ecd51813cf0c1710087
Gyuhyoung Lim <do0ob1124@gmail.com>
2025-03-06 10:50:22 -0500

    Time: 622 ms (18.54%), Space: 62 MB (53.46%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        Set<Integer> set = new HashSet<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
 
-        pq.add(new int[]{0, 0}); // distance, point idx
-        int cost = 0;
-        while(set.size() < n) {
-            int[] currPos = pq.poll();
-            
-            if(set.contains(currPos[1])) continue;
-            
-            cost += currPos[0];            
-            set.add(currPos[1]);
-            
-            int[] point = points[currPos[1]];
-            
+        int n = points.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        Set<Integer> set = new HashSet<>();
+        pq.add(new int[]{0, 0}); // distance, index;
+
+        int result = 0;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int currIdx = curr[1];
+            int currDis = curr[0];
+            if(set.contains(currIdx)) continue;
+            result += currDis;
+            set.add(currIdx);
+
             for(int i = 0; i < n; i++) {
                 if(set.contains(i)) continue;
-                int distance = Math.abs(points[i][0] - point[0]) + Math.abs(points[i][1] - point[1]);
-                pq.add(new int[] {distance, i});
+                int dis = Math.abs(points[i][0] - points[currIdx][0]) + Math.abs(points[i][1] - points[currIdx][1]);
+                pq.add(new int[]{dis, i});
             }
+
         }
 
-        return cost;
+        return result;
         
     }
 }
